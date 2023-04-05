16ea01b64a71f4acf5aca2992a3e65f69fd04129
fabian <javierfabian2107@hotmail.com>
2023-04-05 18:06:40 -0300

    sigue_igual

--- app/apps.js
+++ app/apps.js
@@ -1,27 +1,27 @@
 //menu lateral
 var menu_visible = false;
 let menu = document.getElementById("nav");
-function mostrarOcultarMenu(){
-    if(menu_visible==false){//si esta oculto
+
+function mostrarOcultarMenu() {
+    if (menu_visible == false) { //si esta oculto
         menu.style.display = "block";
         menu_visible = true;
-    }
-    else{
+    } else {
         menu.style.display = "none";
         menu_visible = false;
     }
 }
 //oculto el menu una vez que selecciono una opcion
 let links = document.querySelectorAll("nav a");
-for(var x = 0; x <links.length;x++){
-    links[x].onclick = function(){
-    menu.style.display = "none";
-    menu_visible = false;
+for (var x = 0; x < links.length; x++) {
+    links[x].onclick = function() {
+        menu.style.display = "none";
+        menu_visible = false;
     }
 }
 //creo las barritas de una barra particular identificada por su id
-function crearBarra(id_barra){
-    for(i=0;i<=16;i++){
+function crearBarra(id_barra) {
+    for (i = 0; i <= 16; i++) {
         let div = document.createElement("div");
         div.className = "e";
         id_barra.appendChild(div);
@@ -41,55 +41,55 @@ crearBarra(photoshop);
 let php = document.getElementById("php");
 crearBarra(php);
 let ilustrator = document.getElementById("ilustrator");
-crearBarra(ilustrator); 
+crearBarra(ilustrator);
 
 // a guardar cantidad de barritas que se van a ir pintando por cada barar
 //se usa un array, cada posicion pertenece a un elemneto
 //comienza en -1 porque no tiene ninguna pintada al iniciarse
-let contadores = [-1,-1,-1,-1,-1,-1];
+let contadores = [-1, -1, -1, -1, -1, -1];
 //variable siguiente la voy a utilizar de flag para saber si se ejecuto la animacion
 let entro = false;
 
 //funcion aplica las animaciones de las habilidades
-function efectoHabilidades(){
+function efectoHabilidades() {
     var habilidades = document.getElementById("habilidades");
     var distancia_skills = Window.innerHeight - habilidades.getBoundingClientRect().top;
-    if(distancia_skills>=300 && entro==false){
+    if (distancia_skills >= 300 && entro == false) {
         entro = true;
-        const intervalHtml = setInterval(function(){
+        const intervalHtml = setInterval(function() {
             pintarBarra(html, 16, 0, intervalHtml);
-        },100);
-        const intervalJavascript = setInterval(function(){
-           pintarBarra(javascript, 11, 1, intervalJavascript);
-        },100);
-        const intervalWordpress = setInterval(function(){
-           pintarBarra(wordpress, 11, 2, intervalWordpress);
-        },100);
-        const intervalPhotoshop = setInterval(function(){
+        }, 100);
+        const intervalJavascript = setInterval(function() {
+            pintarBarra(javascript, 11, 1, intervalJavascript);
+        }, 100);
+        const intervalWordpress = setInterval(function() {
+            pintarBarra(wordpress, 11, 2, intervalWordpress);
+        }, 100);
+        const intervalPhotoshop = setInterval(function() {
             pintarBarra(photoshop, 15, 3, intervalPhotoshop);
-        },100);
-        const intervalPhp = setInterval(function(){
+        }, 100);
+        const intervalPhp = setInterval(function() {
             pintarBarra(php, 16, 4, intervalPhp);
-        },100);
-        const intervalIlustrator = setInterval(function(){
+        }, 100);
+        const intervalIlustrator = setInterval(function() {
             pintarBarra(ilustrator, 11, 5, intervalIlustrator);
-        },100);
+        }, 100);
     }
 }
 
 //lleno una barra particular con la cantidad indicada
-function pintarBarra(id_barra, cantidad, indice, interval){
+function pintarBarra(id_barra, cantidad, indice, interval) {
     contadores[indice]++;
     x = contadores[indice];
-    if(x < cantidad){
+    if (x < cantidad) {
         let elementos = id_barra.getElementsByClassName("e");
         elementos[x].style.backgroundColor = "#940253";
-    }else{
+    } else {
         clearInterval(interval)
     }
 }
 
 //detecto el scrolling del mouse para aplicar la animacion de la barra
-window.onscroll = function(){
+window.onscroll = function() {
     efectoHabilidades();
 }
